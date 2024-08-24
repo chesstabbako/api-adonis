000d74a8ff42825b302b2f3f57b9ff1490800b36
Ricardo <ricardojoseajedrez@gmail.com>
2024-08-24 07:56:18 -0400

    users controller

---
+++ app/controllers/users_controller.ts
@@ -0,0 +1,49 @@
+import User from '#models/user'
+import type { HttpContext } from '@adonisjs/core/http'
+
+
+export default class UsersController {
+  /**
+   * Display a list of resource
+   */
+  async index({}: HttpContext) {
+
+    return await User.all()
+  }
+
+  /**
+   * Display form to create a new record
+   */
+
+
+  async create({}: HttpContext) {}
+
+  /**
+   * Handle form submission for the create action
+   */
+  async store({ request }: HttpContext) {
+
+    const user:User= await User.create(request.all())
+    return user;
+  }
+
+  /**
+   * Show individual record
+   */
+  async show({ params }: HttpContext) {}
+
+  /**
+   * Edit individual record
+   */
+  async edit({ params }: HttpContext) {}
+
+  /**
+   * Handle form submission for the edit action
+   */
+  async update({ params, request }: HttpContext) {}
+
+  /**
+   * Delete record
+   */
+  async destroy({ params }: HttpContext) {}
+}
