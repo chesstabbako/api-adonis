839bc06962e4027cba792c66adfde8828ae2cea8
Ricardo <ricardojoseajedrez@gmail.com>
2024-08-25 05:31:57 -0400

    Delete non useful methods

--- app/controllers/users_controller.ts
+++ app/controllers/users_controller.ts
@@ -2,19 +2,7 @@ import User from '#models/user'
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class UsersController {
-  /**
-   * Display a list of resource
-   */
-  async index({}: HttpContext) {
-    return await User.all()
-  }
-
-  /**
-   * Display form to create a new record
-   */
-
-  async create({}: HttpContext) {}
-
+ 
   /**
    * Handle form submission for the create action
    */
@@ -23,27 +11,4 @@ export default class UsersController {
     return user
   }
 
-  /**
-   * Show individual record
-   */
-  async show({ params }: HttpContext) {
-    const user = await User.query().preload('tasks')
-
-    return user
-  }
-
-  /**
-   * Edit individual record
-   */
-  async edit({ params }: HttpContext) {}
-
-  /**
-   * Handle form submission for the edit action
-   */
-  async update({ params, request }: HttpContext) {}
-
-  /**
-   * Delete record
-   */
-  async destroy({ params }: HttpContext) {}
 }
