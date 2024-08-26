ac7bbb729efa1f81b890bb86ab5fb70e2a9b0612
Ricardo <ricardojoseajedrez@gmail.com>
2024-08-25 21:43:10 -0400

    Create method index

--- app/controllers/users_controller.ts
+++ app/controllers/users_controller.ts
@@ -1,11 +1,21 @@
+import Task from '#models/task';
 import User from '#models/user'
 import type { HttpContext } from '@adonisjs/core/http'
+import DB from '@adonisjs/lucid/services/db'
 
 export default class UsersController {
  
   /**
    * Handle form submission for the create action
    */
+
+  async index({ request, auth, response}: HttpContext) {
+    const tasks = await DB.from('tasks').where('user_id', auth?.user?.id)
+    return response.json({
+      tasks,
+    })
+  }
+
   async store({ request }: HttpContext) {
     const user: User = await User.create(request.all())
     return user
